b2480f22b6b87bb0525d49375ab486d6395ac208
Kalle A. Sandström <ksandstr@iki.fi>
2012-02-15 00:36:17 +0200

    instead of replicating <stdint.h>, imitate it
    It's a nonstandard environment with some standard-ish headers. Problem,
    officer?

---
+++ include/fake_clib/stdint.h
@@ -0,0 +1,20 @@
+
+/* minimal, nonconforming <stdint.h> */
+
+#ifndef _STDINT_H
+#define _STDINT_H
+
+#include <stddef.h>
+
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
+typedef int int32_t;
+
+typedef int32_t intptr_t;
+
+
+#endif
--- include/ukernel/types.h
+++ include/ukernel/types.h
@@ -1,23 +1,12 @@
 
-/* type definitions for LP32. these imitate a subset <stdint.h> in a
- * non-hosted environment.
- *
- * add more as required.
+/* this file is basically useless. add "u32", "u16", "u8" and other silly
+ * types if necessary. or rather, remove this file altogether.
  */
 
 #ifndef SEEN_TYPES_H
 #define SEEN_TYPES_H
 
-#include <stddef.h>
+#include <stdint.h>
 
-
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
-
-typedef int int32_t;
-
-typedef int32_t intptr_t;
 
 #endif
