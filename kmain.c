5a84b7d6cd19e4c99b0f1bd7576344ce547faa8b
Kalle A. Sandström <ksandstr@iki.fi>
2012-03-05 23:29:56 +0200

    move struct x86_exregs into <ukernel/x86.h>

--- kmain.c
+++ kmain.c
@@ -18,29 +18,6 @@
 #include "multiboot.h"
 
 
-/* courtesy of L4Ka::pistachio */
-struct x86_exregs {
-	uint32_t reason;
-	uint32_t es;
-	uint32_t ds;
-	uint32_t edi;
-	uint32_t esi;
-	uint32_t ebp;
-	uint32_t __esp;
-	uint32_t ebx;
-	uint32_t edx;
-	uint32_t ecx;
-	uint32_t eax;
-	/* trapgate frame */
-	uint32_t error;
-	uint32_t eip;
-	uint32_t cs;
-	uint32_t eflags;
-	uint32_t esp;
-	uint32_t ss;
-};
-
-
 /* keyboard variables. these are just for testing the PIC setup, and will be
  * replaced with the timer chip's variables soon enough.
  *
