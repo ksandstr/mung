3c282d7b4357eff5f105464e768edea28d610453
Kalle A. Sandström <ksandstr@iki.fi>
2012-03-05 23:28:38 +0200

    add <l4/vregs.h>
    Deviates from L4Ka::Pistachio, so maybe should be moved into <ukernel/vregs.h>
    instead.

---
+++ include/l4/vregs.h
@@ -0,0 +1,19 @@
+
+/* <l4/vregs.h> is not seen in L4Ka::Pistachio. */
+
+#ifndef __L4__VREGS_H__
+#define __L4__VREGS_H__
+
+#include <l4/types.h>
+
+
+#define L4_VREG(base, pos) (((L4_Word_t *)(base))[(pos)])
+
+
+/* names of TCRs and VRs in an UTCB segment, tied to word offsets. */
+
+#define L4_TCR_PAGER (-12)
+#define L4_TCR_MR(n) (n)
+
+
+#endif
