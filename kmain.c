f61e14b2784aef2c0d00d0acf02a3a1a4641acf0
Kalle A. Sandström <ksandstr@iki.fi>
2012-03-12 22:36:48 +0200

    alter pager_thread() to reply with mapitems

--- kmain.c
+++ kmain.c
@@ -514,16 +514,22 @@ void thread_test(void)
 static void pager_thread(void *parameter)
 {
 	printf("pager thread started.\n");
+
+	struct page *zpage = get_kern_page();
+	memset(zpage->vm_addr, 0, PAGE_SIZE);
+
 	for(;;) {
 		L4_ThreadId_t from = L4_anythread;
 		L4_MsgTag_t tag = kipc(L4_nilthread, &from,
 			L4_Timeouts(L4_Never, L4_Never));
-		if(L4_IpcFailed(tag)) {
-			printf("pager ipc failed (no ec yet)\n");
-			continue;
-		}
 
 		for(;;) {
+			if(L4_IpcFailed(tag)) {
+				printf("pager ipc failed (no ec yet)\n");
+				break;
+			}
+
+			L4_ThreadId_t sender = from;
 			void *utcb = thread_get_utcb(get_current_thread());
 			if((tag.X.label & 0xfff0) == 0xffe0) {
 				L4_Word_t fault_addr = L4_VREG(utcb, L4_TCR_MR(1)),
@@ -532,14 +538,24 @@ static void pager_thread(void *parameter)
 					__func__, TID_THREADNUM(from.raw), TID_VERSION(from.raw),
 					fault_addr, fault_ip);
 
-				/* TODO: compose a reply here... */
-				break;
+				L4_VREG(utcb, L4_TCR_MR(0)) = ((L4_MsgTag_t){
+					.X = { .t = 2 } }).raw;
+				/* TODO: map something reasonable in, instead. */
+				L4_MapItem_t map = L4_MapItem(
+					L4_FpageLog2(zpage->id << PAGE_BITS, PAGE_BITS),
+					fault_addr & ~PAGE_MASK);
+				L4_VREG(utcb, L4_TCR_MR(1)) = map.raw[0];
+				L4_VREG(utcb, L4_TCR_MR(2)) = map.raw[1];
 			} else {
 				printf("%s: unknown IPC label 0x%x from %d:%d\n",
 					__func__, tag.X.label, TID_THREADNUM(from.raw),
 					TID_VERSION(from.raw));
 				break;
 			}
+
+			/* ReplyWait */
+			from = L4_anythread;
+			tag = kipc(sender, &from, L4_Timeouts(L4_Never, L4_Never));
 		}
 	}
 }
