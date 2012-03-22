7cf94104300d95914349ce18ff6faea89340f0b2
Kalle A. Sandström <ksandstr@iki.fi>
2012-03-22 20:25:13 +0200

    add L4_TCR_* definitions for the rest of the defined UTCB slots
    Pistachio-style accessors would be nice, too.

--- include/l4/vregs.h
+++ include/l4/vregs.h
@@ -12,7 +12,17 @@
 
 /* names of TCRs and VRs in an UTCB segment, tied to word offsets. */
 
+#define L4_TCR_THREADWORD0 (-4)
+#define L4_TCR_THREADWORD1 (-5)
+#define L4_TCR_VA_SENDER (-6)		/* VirtualSender/ActualSender */
+#define L4_TCR_INTENDEDRECEIVER (-7)
+#define L4_TCR_XFERTIMEOUTS (-8)
+#define L4_TCR_ERRORCODE (-9)
+#define L4_TCR_COP_PREEMPT (-10)	/* COPflags, Preemptflags */
+#define L4_TCR_EXCEPTIONHANDLER (-11)
 #define L4_TCR_PAGER (-12)
+#define L4_TCR_USERDEFINEDHANDLE (-13)
+#define L4_TCR_PROCESSORNO (-14)
 #define L4_TCR_MYGLOBALID (-15)
 #define L4_TCR_MR(n) (n)
 #define L4_TCR_BR(n) (-(n) - 16)
