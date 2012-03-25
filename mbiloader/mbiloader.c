746d9be1f9ee1318e598110133148042b7a67b4b
Kalle A. Sandström <ksandstr@iki.fi>
2012-03-25 21:00:59 +0300

    add serial output, hello-world function to mbiloader
    And make it the program executed by run.sh .

--- mbiloader/mbiloader.c
+++ mbiloader/mbiloader.c
@@ -1,7 +1,81 @@
 
+#include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <ukernel/x86.h>
+#include <ukernel/16550.h>
+
+
+/* rudimentary serial port output from µiX (via kmain.c) */
+#define COM_PORT 0x3f8
+
+void computchar(unsigned char ch)
+{
+//	unsigned int iter = 1;
+
+	/* we'll poll the LSR until the transmit register is empty. */
+	while((inb(COM_PORT + UART_LSR) & UART_LSR_ETHR) == 0) {
+#if 0
+		/* yield every 128k iterations. that's far more than the time required
+		 * to transmit one byte even over a 2400bps line.
+		 */
+		if((++iter & (128*1024-1)) == 0) L4_Yield();
+#endif
+	}
+	outb(COM_PORT + UART_RDWR, ch);
+	/* and then poll again until the holding register is empty, i.e. until
+	 * the character has really been transmitted.
+	 *
+	 * (yeah, a proper serial driver would use the FIFO. no, this is quite
+	 * enough for now thank you.)
+	 */
+//	iter = 1;
+	while((inb(COM_PORT + UART_LSR) & UART_LSR_EDHR) == 0) {
+//		if((++iter & (128*1024-1)) == 0) L4_Yield();
+	}
+	if(ch == '\n') computchar('\r');
+}
+
+
+void putstr(const char *str)
+{
+	while(*str != '\0') computchar(*(str++));
+}
+
+
+void NORETURN panic(const char *message)
+{
+	printf("PANIC: %s\n", message);
+	while(true) {
+		asm("cli; hlt");
+	}
+}
+
+
+#ifndef NDEBUG
+void __assert_failure(
+	const char *condition,
+	const char *file,
+	unsigned int line,
+	const char *function)
+{
+	printf("assert(%s) failed in `%s' (%s:%u)\n", condition, function,
+		file, line);
+	panic("*** assertion failure");
+}
+#endif
+
+
+void *malloc(size_t size) {
+	panic("malloc(3) not implemented in mbiloader");
+}
+
 
 int bootmain(void *mbi, uint32_t magic)
 {
+	printf("mbiloader says hello!\n");
+
 	return 0;
 }
--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-exec kvm -serial stdio -no-reboot -net none -kernel image.bin -initrd user/sigma0 $@
+
+#exec kvm -serial stdio -no-reboot -net none -kernel image.bin -initrd user/sigma0 $@
+
+exec kvm -serial stdio -no-reboot -net none -kernel mbiloader/mbiloader -initrd "image.bin,user/sigma0" $@
