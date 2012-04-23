f72f4fb25615d72a2dbd6f987d8f13122ef6a0da
Kalle A. Sandström <ksandstr@iki.fi>
2012-04-24 02:02:19 +0300

    add untested serial I/O to testbench
    Untested because there's no support for I/O from userspace tasks yet. I/O
    pagefaults are up next.

--- user/testbench/testbench.c
+++ user/testbench/testbench.c
@@ -4,9 +4,14 @@
  * and so forth.
  */
 
+#include <stdio.h>
+
 #include <l4/types.h>
 #include <l4/ipc.h>
 #include <l4/syscall.h>
+
+#include <ukernel/16550.h>
+#include <ukernel/ioport.h>
 
 
 void abort(void)
@@ -19,9 +24,42 @@ void abort(void)
 }
 
 
-void con_putstr(const char *str)
+/* XXX this code is copypasta'd from kmain.c, and should be moved into a
+ * common "ser_io.c" or some such.
+ */
+/* rudimentary serial port output from µiX */
+#define COM_PORT 0x3f8
+
+static void computchar(unsigned char ch)
 {
-	/* TODO: add serial I/O */
+//	unsigned int iter = 1;
+
+	/* we'll poll the LSR until the transmit register is empty. */
+	while((inb(COM_PORT + UART_LSR) & UART_LSR_ETHR) == 0) {
+#if 0
+		/* yield every 128k iterations. that's far more than the time required
+		 * to transmit one byte even over a 2400bps line.
+		 */
+		if((++iter & (128*1024-1)) == 0) L4_Yield();
+#endif
+	}
+	outb(COM_PORT + UART_RDWR, ch);
+	/* and then poll again until the holding register is empty, i.e. until
+	 * the character has really been transmitted.
+	 *
+	 * (yeah, a proper serial driver would use the FIFO. no, this is quite
+	 * enough for now thank you.)
+	 */
+//	iter = 1;
+	while((inb(COM_PORT + UART_LSR) & UART_LSR_EDHR) == 0) {
+//		if((++iter & (128*1024-1)) == 0) L4_Yield();
+	}
+	if(ch == '\n') computchar('\r');
+}
+
+
+void con_putstr(const char *str) {
+	while(*str != '\0') computchar(*(str++));
 }
 
 
@@ -42,5 +80,7 @@ void __assert_failure(
 
 int main(void)
 {
+	printf("hello, world!\n");
+
 	return 0;
 }
