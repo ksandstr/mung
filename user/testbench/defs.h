3940c7779fc1d2051dc07e01b7588c66b831722d
Kalle A. Sandström <ksandstr@iki.fi>
2012-05-06 23:00:20 +0300

    add unit testing macros & _fail_unless() to testbench defs.h

--- user/testbench/defs.h
+++ user/testbench/defs.h
@@ -2,6 +2,44 @@
 #ifndef SEEN_TESTBENCH_DEFS_H
 #define SEEN_TESTBENCH_DEFS_H
 
+#include <stdio.h>
+#include <stdarg.h>
+
+
+/* unit testing things */
+#define fail(...) _fail_unless(0, __FILE__, __LINE__, "Failed", ## __VA_ARGS__, NULL)
+
+#define fail_unless(expr, ...) \
+	_fail_unless(expr, __FILE__, __LINE__, \
+		"Assertion `" #expr "' failed", ## __VA_ARGS__, NULL)
+
+#define fail_if(expr, ...) \
+	_fail_unless(!(expr), __FILE__, __LINE__, \
+		"Failure `" #expr "' occurred", ## __VA_ARGS__, NULL)
+
+/* (this could live in a module.) */
+static inline void _fail_unless(
+	int result,
+	const char *file,
+	int line,
+	const char *expr,
+	...)
+{
+	if(!result) {
+		va_list ap;
+		char buf[512];
+		va_start(ap, expr);
+		const char *msg = va_arg(ap, char *);
+		if(msg == NULL) msg = expr;
+		vsnprintf(buf, sizeof(buf), msg, ap);
+		va_end(ap);
+		printf("FAIL: %s\n", buf);
+
+		/* meh */
+		asm volatile ("int $1");
+	}
+}
+
 
 extern void threadctl_test(void);
 extern void sched_test(void);
