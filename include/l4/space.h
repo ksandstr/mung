4a7eb29615ea067cc18e2ff6a3bad87c1514aa86
Kalle A. Sandström <ksandstr@iki.fi>
2012-08-13 00:50:07 +0300

    add <l4/space.h>
    It omits the C++ bits as those aren't testable in this source tree.

---
+++ include/l4/space.h
@@ -0,0 +1,69 @@
+
+#ifndef __L4__SPACE_H__
+#define __L4__SPACE_H__
+
+#include <stdbool.h>
+
+#include <l4/types.h>
+#include <l4/syscall.h>
+
+
+static inline L4_Fpage_t L4_UnmapFpage(L4_Fpage_t f)
+{
+	L4_LoadMR(0, f.raw);
+	L4_Unmap(0);
+	L4_StoreMR(0, &f.raw);
+	return f;
+}
+
+
+static inline void L4_UnmapFpages(L4_Word_t n, L4_Fpage_t *fpages)
+{
+	L4_LoadMRs(0, n, (L4_Word_t *)fpages);
+	L4_Unmap(n - 1);
+	L4_StoreMRs(0, n, (L4_Word_t *)fpages);
+}
+
+
+static inline L4_Fpage_t L4_FlushFpage(L4_Fpage_t f)
+{
+	L4_LoadMR(0, f.raw);
+	L4_Unmap(0x40);
+	L4_StoreMR(0, &f.raw);
+	return f;
+}
+
+
+static inline void L4_FlushFpages(L4_Word_t n, L4_Fpage_t *fpages)
+{
+	L4_LoadMRs(0, n, (L4_Word_t *)fpages);
+	L4_Unmap(0x40 | (n - 1));
+	L4_StoreMRs(0, n, (L4_Word_t *)fpages);
+}
+
+
+static inline L4_Fpage_t L4_GetStatus(L4_Fpage_t f)
+{
+	L4_LoadMR(0, f.raw & ~0x0f);
+	L4_Unmap(0);
+	L4_StoreMR(0, &f.raw);
+	return f;
+}
+
+
+static inline bool L4_WasWritten(L4_Fpage_t f) {
+	return (f.raw & L4_Writable) != 0;
+}
+
+
+static inline bool L4_WasReferenced(L4_Fpage_t f) {
+	return (f.raw & L4_Readable) != 0;
+}
+
+
+static inline bool L4_WaseXecuted(L4_Fpage_t f) {
+	return (f.raw & L4_eXecutable) != 0;
+}
+
+
+#endif
