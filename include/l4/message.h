bbcf67937f1274cf7c8713caf307b2ce011f8903
Kalle A. Sandström <ksandstr@iki.fi>
2012-12-04 00:54:44 +0200

    add L4_StringItem_t to <l4/message.h>
    And its inline functions. Only the C parts, though.
    
    These are a mild rewrite (and therefore a direct derivative work) of the
    L4Ka::Pistachio <l4/message.h>'s related bits.

--- include/l4/message.h
+++ include/l4/message.h
@@ -89,4 +89,94 @@ static inline L4_Word_t L4_GrantItemSndBase(L4_GrantItem_t m) {
 }
 
 
+/* string item */
+
+typedef union {
+	L4_Word_t raw[2];
+	struct {
+		L4_Word_t C:1;
+		L4_Word_t __type:3;
+		L4_Word_t j:5;
+		L4_Word_t c:1;
+		L4_Word_t string_length:22;
+		union {
+			void *string_ptr;
+			void *substring_ptr[0];
+		} str;
+	} X;
+} L4_StringItem_t;
+
+
+static inline L4_StringItem_t L4_StringItem(int size, void *address) {
+	return (L4_StringItem_t){
+		.X.string_length = size, .X.str.string_ptr = address,
+	};
+}
+
+
+static inline bool L4_IsStringItem(L4_StringItem_t *s) {
+	return (s->X.__type & 0x04) == 0;
+}
+
+
+static inline bool L4_CompoundString(L4_StringItem_t *s) {
+	return s->X.c != 0;
+}
+
+
+static inline L4_Word_t L4_Substrings(L4_StringItem_t *s) {
+	return s->X.j + 1;
+}
+
+
+static inline void *L4_Substring(L4_StringItem_t *s, L4_Word_t n) {
+	return s->X.str.substring_ptr[n - 1];
+}
+
+
+static inline L4_StringItem_t *__L4_EndOfString(
+	L4_StringItem_t *s,
+	L4_StringItem_t **p)
+{
+	L4_StringItem_t *prev;
+	do {
+		prev = s;
+		s = (L4_StringItem_t *)&s->X.str.substring_ptr[s->X.j + 1];
+	} while(prev->X.c);
+	if(p != NULL) *p = prev;
+	return s;
+}
+
+
+static inline void __L4_Copy_String(L4_StringItem_t *d, L4_StringItem_t *s)
+{
+	L4_Word_t *dest = (L4_Word_t *)d, *from = (L4_Word_t *)s,
+		*end = (L4_Word_t *)__L4_EndOfString(s, (L4_StringItem_t **)0);
+	while(from < end) *(dest++) = *(from++);
+}
+
+
+static inline L4_StringItem_t *L4_AddSubstringTo(
+	L4_StringItem_t *dest,
+	L4_StringItem_t *substr)
+{
+	L4_StringItem_t *prev;
+	dest = __L4_EndOfString(dest, &prev);
+	prev->X.c = 1;
+	__L4_Copy_String(dest, substr);
+	return dest;
+}
+
+
+static inline L4_StringItem_t *L4_AddSubstringAddressTo(
+	L4_StringItem_t *dest,
+	void *substring_addr)
+{
+	L4_StringItem_t *last;
+	__L4_EndOfString(dest, &last);
+	last->X.str.substring_ptr[++last->X.j] = substring_addr;
+	return dest;
+}
+
+
 #endif
