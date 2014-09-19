c77679d7c380792e57043801573f8e1e038aa338
Kalle A. Sandström <ksandstr@iki.fi>
2014-09-19 23:33:56 +0300

    fake_clib: add extern prototype for vfprintf()

--- include/fake_clib/stdio.h
+++ include/fake_clib/stdio.h
@@ -21,6 +21,8 @@ extern int printf(const char *fmt, ...)
 extern int fprintf(FILE *stream, const char *fmt, ...)
 	__attribute__((format(printf, 2, 3)));
 
+extern int vfprintf(FILE *stream, const char *fmt, va_list args);
+
 extern int snprintf(char *buf, size_t size, const char *fmt, ...)
 	__attribute__((format(printf, 3, 4)));
 
