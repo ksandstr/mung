189de1f900399e65145eddb5c157f2cf20c339c2
Kalle A. Sandström <ksandstr@iki.fi>
2015-07-11 22:55:44 +0300

    l4/types.h: allow denormal addresses in L4_Fpage_t ctors
    sigma0_stub.c generates these in the L4_Sigma0_GetAny() routine.

--- include/l4/types.h
+++ include/l4/types.h
@@ -144,9 +144,7 @@ static inline L4_Bool_t L4_IsNilFpage(L4_Fpage_t fp) {
 	return fp.raw == 0;
 }
 
-static inline L4_Fpage_t L4_FpageLog2(L4_Word_t address, int shift)
-{
-	_L4_ASSERT((address & ((1 << shift) - 1)) == 0);
+static inline L4_Fpage_t L4_FpageLog2(L4_Word_t address, int shift) {
 	return (L4_Fpage_t){ .X = { .s = shift, .b = address >> 10 } };
 }
 
@@ -154,7 +152,6 @@ static inline L4_Fpage_t L4_Fpage(L4_Word_t address, L4_Word_t size) {
 	/* GCC intrinsics. */
 	int msb = sizeof(L4_Word_t) * 8 - __builtin_clzl(size) - 1,
 		shift = (1ul << msb) <= size ? msb : msb + 1;
-	_L4_ASSERT((address & ((1 << shift) - 1)) == 0);
 	return (L4_Fpage_t){ .X = { .s = shift, .b = address >> 10 } };
 }
 
