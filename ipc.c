a0351f6d84526cfb4d23e1df7bfea588c0e5c73c
Kalle A. Sandström <ksandstr@iki.fi>
2016-05-06 00:18:25 +0300

    ipc: account for delayed report of invalid Ipc.FromSpec
    That's to say, fix an assert in ipc_recv_half(). The way this condition
    happens is that the stored FromSpec becomes invalid either during passive
    send, or in the latency before the scheduler executes the receive phase once
    the passive send completes.

--- ipc.c
+++ ipc.c
@@ -1443,7 +1443,7 @@ bool ipc_recv_half(struct thread *self, void *self_utcb)
 
 err_no_partner:
 	set_ipc_error(self_utcb, 5);
-	assert(self->status == TS_RUNNING);
+	assert(self->status == TS_RUNNING || self->status == TS_R_RECV);
 	self->status = TS_READY;	/* failed active receive -> READY. */
 	return false;
 }
