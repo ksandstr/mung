00964eb55d73d19193c3635e36a6262c4bf647e0
Kalle A. Sandström <ksandstr@iki.fi>
2016-11-12 23:50:31 +0200

    add -Wno-frame-address to config.mk
    Turst me, I know what I'm oing.

--- config.mk
+++ config.mk
@@ -10,6 +10,7 @@ CFLAGS=-O2 -Wall -march=native -std=gnu99 -m32 \
 	-D_GNU_SOURCE -D__KERNEL__ -DENABLE_SELFTEST \
 	-mno-avx -mno-sse2 -fno-pic -fuse-ld=gold \
 	-fno-builtin -nostdlib \
+	-Wno-frame-address \
 	#-DDEBUG_ME_HARDER #-D_L4_DEBUG_ME_HARDER #-DCCAN_LIST_DEBUG
 
 MUIDL:=$(abspath $(MUIDL_DIR)/muidl)
