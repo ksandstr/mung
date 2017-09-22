3e3d96d47bfeb89355b2de9505ccd3a1dafb55fd
Kalle A. Sandström <ksandstr@iki.fi>
2017-09-23 01:01:06 +0300

    add tss_* and some of thrd_* to <threads.h>
    None of those are implemented here, for now, but the prototypes are useful
    to have in that file.

--- include/fake_clib/threads.h
+++ include/fake_clib/threads.h
@@ -8,6 +8,7 @@
 #define SEEN_FAKE_CLIB_THREADS_H
 
 #include <stdbool.h>
+#include <stdatomic.h>
 #include <l4/types.h>
 
 
@@ -29,11 +30,26 @@ enum {
 
 struct timespec;	/* left undefined here */
 
+typedef void (*tss_dtor_t)(void *);
+typedef int tss_t;	/* may as well. */
+
+typedef int (*thrd_start_t)(void *);
+typedef int thrd_t;
+
+
+#define ONCE_FLAG_INIT 0
+
+typedef _Atomic int once_flag;
+extern void call_once(once_flag *flag, void (*func)(void));
+
 
 /* C11 threads. */
 
 typedef int thrd_t;
 extern thrd_t thrd_current(void);
+extern int thrd_create(thrd_t *thr, thrd_start_t fn, void *arg);
+extern void thrd_exit(int res);
+extern int thrd_join(thrd_t thr, int *res_p);
 
 /* non-portable outside the testbench runtime. */
 extern int thrd_set_daemon_NP(thrd_t thread, bool is_daemon);
@@ -47,6 +63,16 @@ extern int mtx_lock(mtx_t *mtx);
 extern int mtx_trylock(mtx_t *mtx);
 extern int mtx_timedlock(mtx_t *mtx, const struct timespec *ts);
 extern int mtx_unlock(mtx_t *mtx);
+
+
+/* C11 explicit per-thread data. (not implemented in mung or its testbench
+ * runtime. declared here for completeness.)
+ */
+
+extern int tss_create(tss_t *key, tss_dtor_t dtor);
+extern void tss_delete(tss_t key);
+extern void *tss_get(tss_t key);
+extern void tss_set(tss_t key, void *ptr);
 
 
 #endif
