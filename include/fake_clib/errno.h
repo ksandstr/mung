c46bf2a2e036eba9c2dfdb6def3ed589c79da307
Kalle A. Sandström <ksandstr@iki.fi>
2017-12-13 14:12:36 +0200

    lib: add EBUSY to <errno.h>
    Not used in this tree, but elsewhere.

--- include/fake_clib/errno.h
+++ include/fake_clib/errno.h
@@ -9,6 +9,7 @@
 #define ECHILD	10	/* no child processes */
 #define ENOMEM	12	/* out of memory */
 #define EFAULT	14	/* bad address */
+#define EBUSY	16	/* device or resource busy */
 #define EEXIST	17	/* file exists */
 #define EINVAL	22	/* invalid value */
 #define ENOSYS	28	/* function not implemented */
