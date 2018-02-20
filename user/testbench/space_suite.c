92a60c6cdfcf321dd7d6b4c8ec93d117fa2632d5
Kalle A. Sandström <ksandstr@iki.fi>
2018-02-20 02:27:51 +0200

    test: space: add first_page_empty_overmap
    Hits a case but doesn't examine the entrails. This one doesn't depend on
    holeunmap behaviour.

--- user/testbench/space_suite.c
+++ user/testbench/space_suite.c
@@ -1106,6 +1106,26 @@ START_LOOP_TEST(huge_fanout, iter, 0, 1)
 END_TEST
 
 
+static int send_empty(L4_ThreadId_t peer)
+{
+	void *empty = aligned_alloc(PAGE_SIZE, PAGE_SIZE);
+	diag("empty=%p", empty);
+	fail_if(empty == NULL);
+	memset(empty, 0xff, PAGE_SIZE);
+	L4_Fpage_t e = L4_FpageLog2((L4_Word_t)empty, PAGE_BITS);
+	L4_Set_Rights(&e, L4_FullyAccessible);
+	L4_FlushFpage(e);
+	L4_MapItem_t mi = L4_MapItem(e, 0);
+	L4_LoadMR(0, (L4_MsgTag_t){ .X.t = 2 }.raw);
+	L4_LoadMRs(1, 2, mi.raw);
+	L4_MsgTag_t tag = L4_Call_Timeouts(peer, TEST_IPC_DELAY, TEST_IPC_DELAY);
+	L4_FlushFpage(e);
+	free(empty);
+
+	return L4_IpcSucceeded(tag) ? 0 : 1;
+}
+
+
 /* map an empty page over the last page in a 4M segment.
  *
  * variables:
@@ -1129,19 +1149,7 @@ START_LOOP_TEST(last_page_empty_overmap, iter, 0, 1)
 	L4_ThreadId_t parent_tid = L4_Myself(), child_tid;
 	int cpid = fork_tid(&child_tid);
 	if(cpid == 0) {
-		void *empty = aligned_alloc(PAGE_SIZE, PAGE_SIZE);
-		diag("empty=%p", empty);
-		fail_if(empty == NULL);
-		memset(empty, 0xff, PAGE_SIZE);
-		L4_Fpage_t e = L4_FpageLog2((L4_Word_t)empty, PAGE_BITS);
-		L4_Set_Rights(&e, L4_FullyAccessible);
-		L4_FlushFpage(e);
-		L4_MapItem_t mi = L4_MapItem(e, 0);
-		L4_LoadMR(0, (L4_MsgTag_t){ .X.t = 2 }.raw);
-		L4_LoadMRs(1, 2, mi.raw);
-		L4_Call_Timeouts(parent_tid, TEST_IPC_DELAY, TEST_IPC_DELAY);
-
-		exit(0);
+		exit(send_empty(parent_tid));
 	}
 
 	void *arena = aligned_alloc(group_size, group_size);
@@ -1171,6 +1179,45 @@ START_LOOP_TEST(last_page_empty_overmap, iter, 0, 1)
 	int st, dead = wait(&st);
 	fail_if(dead != cpid);
 
+	free(arena);
+}
+END_TEST
+
+
+/* map an empty page into an empty 4M range. */
+START_TEST(first_page_empty_overmap)
+{
+	const size_t group_size = 4 * 1024 * 1024;
+	diag("group_size=%#lx", (unsigned long)group_size);
+
+	plan_tests(3);
+
+	L4_ThreadId_t parent_tid = L4_Myself(), child_tid;
+	int cpid = fork_tid(&child_tid);
+	if(cpid == 0) {
+		exit(send_empty(parent_tid));
+	}
+
+	void *arena = aligned_alloc(group_size, group_size);
+	diag("arena=%p", arena);
+	memset(arena, 0xc3, group_size);
+	/* flush it all. */
+	L4_Fpage_t a_page = L4_Fpage((L4_Word_t)arena, group_size);
+	L4_Set_Rights(&a_page, L4_FullyAccessible);
+	L4_FlushFpage(a_page);
+
+	/* receive into it, anywhere. */
+	L4_Acceptor_t acc = L4_MapGrantItems(a_page);
+	L4_Accept(acc);
+	L4_MsgTag_t tag = L4_Receive_Timeout(child_tid, TEST_IPC_DELAY);
+	pass("still alive");
+	ok(L4_IpcSucceeded(tag), "receive from child");
+	ok1(L4_TypedWords(tag) == 2);
+	L4_LoadMR(0, 0);
+	L4_Reply(child_tid);
+
+	int st, dead = wait(&st);
+	fail_if(dead != cpid);
 	free(arena);
 }
 END_TEST
@@ -1314,6 +1361,7 @@ static Suite *space_suite(void)
 		tcase_add_test(tc, mapdb_shrimps);
 		tcase_add_test(tc, huge_fanout);
 		tcase_add_test(tc, last_page_empty_overmap);
+		tcase_add_test(tc, first_page_empty_overmap);
 		suite_add_tcase(s, tc);
 	}
 
