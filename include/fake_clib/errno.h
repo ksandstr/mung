98680e69340ff10a16ea476490dc73be9ad00653
Kalle A. Sandström <ksandstr@iki.fi>
2018-06-18 02:24:32 +0300

    lib: add EAGAIN, EPERM, ESRCH

--- include/fake_clib/errno.h
+++ include/fake_clib/errno.h
@@ -2,13 +2,16 @@
 #ifndef _ERRNO_H
 #define _ERRNO_H
 
-/* pull error codes from µiX or somewhere as required... */
+/* pull error codes from wherever, as required... */
 
+#define EPERM	1	/* operation not permitted */
 #define ENOENT	2	/* no such file or directory */
+#define ESRCH	3	/* no such process */
 #define EIO		5	/* i/o error */
 #define E2BIG	7	/* argument list too long */
 #define EBADF	9	/* bad file number */
 #define ECHILD	10	/* no child processes */
+#define EAGAIN	11	/* try again */
 #define ENOMEM	12	/* out of memory */
 #define EFAULT	14	/* bad address */
 #define EBUSY	16	/* device or resource busy */
