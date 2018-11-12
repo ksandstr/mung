9ee1017b2ee7053c506bc29b59532c0fa1e11260
Kalle A. Sandström <ksandstr@iki.fi>
2018-11-12 17:25:25 +0200

    use KVM where it exists
    Traditional emulation elsewhere.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # this script is used by user/testbench/report.pl .
-exec kvm -serial stdio -no-reboot -net none -kernel mbiloader/mbiloader -initrd "ia32-kernel,user/sigma0,user/testbench/testbench,user/testbench/forkserv $TESTBENCH_OPTS" $@
+KVM=""
+if [ -d "/sys/module/kvm" ]; then KVM="-enable-kvm"; fi
+exec qemu-system-i386 ${KVM} -serial stdio -no-reboot -net none -kernel mbiloader/mbiloader -initrd "ia32-kernel,user/sigma0,user/testbench/testbench,user/testbench/forkserv $TESTBENCH_OPTS" $@
